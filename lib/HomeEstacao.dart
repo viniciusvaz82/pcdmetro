11bd67cb8ffef13ff9cb77fdc870b8342b94e4bc
viniciusvaz82 <43301111+viniciusvaz82@users.noreply.github.com>
2022-08-31 16:15:33 -0300

    telas cadastro pcd estacao

---
+++ lib/HomeEstacao.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+import 'estacaoOcorrenciaPcd.dart';
+
+class HomeEstacao extends StatefulWidget {
+  @override
+  State<HomeEstacao> createState() => _HomeEstacaoState();
+}
+
+class _HomeEstacaoState extends State<HomeEstacao> {
+  TextEditingController sentido2 = new TextEditingController();
+  TextEditingController sentido1 = new TextEditingController();
+  var alpha = 0.5;
+  var ocorrencia = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: Colors.transparent,
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/fundo.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const SizedBox(
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "Estação - Sé", //aqui tem que puxar da informação do login
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic),
+                ),
+              ),
+            ),
+            SizedBox(
+              //Box de Sentido1
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  minLines: 3,
+                  maxLines: 3,
+                  controller: sentido1,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(4)),
+                      filled: true,
+                      enabled: false,
+                      fillColor: Colors.blueAccent,
+                      labelText: 'Aguardando entrada PCDs. . .',
+                      labelStyle: const TextStyle(
+                        color: Color.fromARGB(255, 231, 229, 229),
+                        fontStyle: FontStyle.italic,
+                      )),
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic),
+                ),
+              ),
+            ),
+            SizedBox(
+              //Box de Sentido2
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  minLines: 3,
+                  maxLines: 3,
+                  controller: sentido2,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(4)),
+                      filled: true,
+                      enabled: false,
+                      fillColor: Colors.blueAccent,
+                      labelText: 'Aguardando entrada PCDs. . .',
+                      labelStyle: const TextStyle(
+                        color: Color.fromARGB(255, 231, 229, 229),
+                        fontStyle: FontStyle.italic,
+                      )),
+                  textAlign: TextAlign.left,
+                  style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 10,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic),
+                ),
+              ),
+            ),
+            SizedBox(
+              //Box de Sentido2
+              width: 500,
+              child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextButton(
+                      onPressed: () {},
+                      child: Text("Confirmar PCDs",
+                          style: TextStyle(color: Colors.white)),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color.fromARGB(255, 74, 159, 0))))),
+            ),
+            InkWell(
+              child: Container(
+                //Box Cadeirante Motorizada
+                width: 500,
+                height: 60,
+                //alignment: Alignment.topLeft,
+                decoration: const BoxDecoration(
+                    color: Color.fromRGBO(0, 76, 159, 1.000)),
+                child: const Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Image(
+                    image: AssetImage(
+                      "assets/images/motorizada_cc.png",
+                    ),
+                    //opacity: ,
+                    fit: BoxFit.contain,
+                    width: 200,
+                  ),
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => estacaoOcorrencia("motorizada")));
+                print("teste motorizada");
+              },
+            ),
+            InkWell(
+              child: Container(
+                //Box Def Visual
+                width: 500,
+                height: 60,
+                //alignment: Alignment.topLeft,
+                decoration: const BoxDecoration(
+                    color: Color.fromRGBO(0, 76, 159, 1.000)),
+                child: const Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Image(
+                    image: AssetImage(
+                      "assets/images/deficientevisual_cc.png",
+                    ),
+                    //opacity: ,
+                    fit: BoxFit.contain,
+                    width: 200,
+                  ),
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => estacaoOcorrencia("visual")));
+                print("teste visual");
+              },
+            ),
+            InkWell(
+              child: Container(
+                //Box Cadeirante
+                width: 500,
+                height: 60,
+                //alignment: Alignment.topLeft,
+                decoration: const BoxDecoration(
+                    color: Color.fromRGBO(0, 76, 159, 1.000)),
+                child: const Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Image(
+                    image: AssetImage(
+                      "assets/images/cadeirante_cc.png",
+                    ),
+                    //opacity: ,
+                    fit: BoxFit.contain,
+                    width: 200,
+                  ),
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => estacaoOcorrencia("cadeirante")));
+                print("teste cadeirante");
+              },
+            ),
+            InkWell(
+              child: Container(
+                //Box Outros
+                width: 500,
+                height: 60,
+                //alignment: Alignment.topLeft,
+                decoration: const BoxDecoration(
+                    color: Color.fromRGBO(0, 76, 159, 1.000)),
+                child: const Padding(
+                  padding: EdgeInsets.all(10),
+                  child: Image(
+                    image: AssetImage(
+                      "assets/images/deficienteoutros_cc.png",
+                    ),
+                    //opacity: ,
+                    fit: BoxFit.contain,
+                    width: 200,
+                  ),
+                ),
+              ),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => estacaoOcorrencia("outros")));
+                print("teste outros tipos");
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/estacaoOcorrenciaPcd.dart
@@ -0,0 +1,175 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'dart:io';
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:pcdmetro/HomeEstacao.dart';
+
+class estacaoOcorrencia extends StatefulWidget {
+  //const estacaoOcorrencia({Key? key}) : super(key: key);
+  String tipo;
+  estacaoOcorrencia(this.tipo);
+  @override
+  State<estacaoOcorrencia> createState() => _estacaoOcorrenciaState();
+}
+
+class _estacaoOcorrenciaState extends State<estacaoOcorrencia> {
+  TextEditingController nome = new TextEditingController();
+  TextEditingController desembarque = new TextEditingController();
+  TextEditingController especial = new TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      //backgroundColor: Colors.transparent,
+      body: Container(
+        constraints: BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/images/fundo.png"),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            const SizedBox(
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "Cadastro de PCD", //aqui tem que puxar da informação do login
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic),
+                ),
+              ),
+            ),
+            SizedBox(
+              //teste trazendo valor da tela anterior
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  "Tipo: ${widget.tipo}", //aqui tem que puxar da informação do login
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w700,
+                      fontStyle: FontStyle.italic),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: nome,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Nome. . .',
+                      //counterText: '',
+                      //contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      labelStyle: TextStyle(
+                        color: Color.fromARGB(255, 205, 203, 203),
+                        //fontSize: 22,
+                        fontStyle: FontStyle.italic,
+                        //decoration: TextDecoration.underline
+                      )),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 500,
+              child: Padding(
+                padding: EdgeInsets.all(10),
+                child: TextFormField(
+                  controller: desembarque,
+                  decoration: InputDecoration(
+                      border: UnderlineInputBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Desembarque. . .',
+                      //counterText: '',
+                      //contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      labelStyle: TextStyle(
+                        color: Color.fromARGB(255, 205, 203, 203),
+                        //fontSize: 22,
+                        fontStyle: FontStyle.italic,
+                        //decoration: TextDecoration.underline
+                      )),
+                ),
+              ),
+            ),
+            if (widget.tipo == "outros") ...{
+              SizedBox(
+                width: 500,
+                child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextFormField(
+                    controller: especial,
+                    decoration: InputDecoration(
+                        border: UnderlineInputBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        filled: true,
+                        fillColor: Colors.white,
+                        labelText: 'Necessidade especial. . .',
+                        //counterText: '',
+                        //contentPadding: const EdgeInsets.symmetric(horizontal: 10.0),
+                        labelStyle: TextStyle(
+                          color: Color.fromARGB(255, 205, 203, 203),
+                          //fontSize: 22,
+                          fontStyle: FontStyle.italic,
+                          //decoration: TextDecoration.underline
+                        )),
+                  ),
+                ),
+              )
+            },
+            SizedBox(
+              //botão salvar
+              width: 500,
+              child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextButton(
+                      onPressed: () {},
+                      child:
+                          Text("Salvar", style: TextStyle(color: Colors.white)),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color.fromARGB(255, 74, 159, 0))))),
+            ),
+            SizedBox(
+              //botão voltar
+              width: 500,
+              child: Padding(
+                  padding: EdgeInsets.all(10),
+                  child: TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomeEstacao()));
+                      },
+                      child:
+                          Text("Voltar", style: TextStyle(color: Colors.white)),
+                      style: ButtonStyle(
+                          backgroundColor: MaterialStateProperty.all(
+                              Color.fromARGB(255, 74, 159, 0))))),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
